952ba4e3534def479658542290721c185b0eedf4
jan <jan.handy16@gmail.com>
2021-09-25 00:53:05 +0200

    add modal hinzugefügt

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalPage } from '../modal/modal.page';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,18 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(public modalController: ModalController) {}
 
-}
+  async loadModal(){
+    console.log("test");
+    var modalController  : ModalController;
+    const modal = await this.modalController.create({
+      component: ModalPage,
+    });
+
+    modal.present();
+  
+  } ;
+  
+};  
+
