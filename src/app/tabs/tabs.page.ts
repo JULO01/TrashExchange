90176b20c41807dde8882019e1b6599654ff44fa
Leon <l.kniffki@t-online.de>
2021-09-25 17:12:59 +0200

    Login modal closeable

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { LoginPage } from '../login/login.page';
 import { ModalPage } from '../modal/modal.page';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {
 
-  constructor(private modalController: ModalController) {
-    this.login()
+@Injectable()
+export class TabsPage {
+  public uid: string = "";
+
+  constructor(public auth: AngularFireAuth, private modalController: ModalController) {
+    this.auth.user.subscribe(user => {
+      if(user) {
+        this.uid = user.uid;
+      } else {
+        this.login();
+      }
+    })
   }
+
 
   async login() {
     const modal = await this.modalController.create({
@@ -35,10 +46,5 @@ export class TabsPage {
     modal.present();
   
   };
-
-  public close() {
-    this.modalController.dismiss();
-  }
   
-};  
-
+};
