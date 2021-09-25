ebb78cac382780f52a7b4f2001ea3cd006e22fb4
JULO01 <90795588+JULO01@users.noreply.github.com>
2021-09-25 15:59:24 +0200

    Like Funktionalität eingefügt

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonInfiniteScroll } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -12,7 +14,33 @@ import { IonInfiniteScroll } from '@ionic/angular';
 export class Tab1Page {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
-  constructor() {}
+
+  items: Observable <any[]>;
+  userid: Observable <any[]>;
+
+  heartName = "heart-outline";
+
+  constructor(firestore:AngularFirestore) {
+    this.items = firestore.collection('angebote').valueChanges();
+    this.userid = null ;
+  }
+
+
+  heartButtonPushed(itemId){
+  }
+  
+
+  changeState(){ 
+    
+    if (this.heartName == "heart-outline"){
+      this.heartName = "heart"
+    }
+    else{
+      this.heartName = "heart-outline"
+    }
+  }
+
+
 
 
 }
---
+++ src/environments/firebase.ts
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyAkKWdpWDQRKBiZJzE8jpnEuBWojjEatIE",
+  
+    authDomain: "trashxchange-2fd99.firebaseapp.com",
+  
+    projectId: "trashxchange-2fd99",
+  
+    storageBucket: "trashxchange-2fd99.appspot.com",
+  
+    messagingSenderId: "674446888649",
+  
+    appId: "1:674446888649:web:6bce97f9089eb5fa484718"
+  
+  };
+  
