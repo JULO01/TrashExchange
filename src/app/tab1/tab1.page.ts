5bc287231e86882e225f717d3d6c44ede69aabac
JULO01 <90795588+JULO01@users.noreply.github.com>
2021-09-25 13:07:04 +0200

    Karte für Objekte erstellt

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,20 +14,5 @@ export class Tab1Page {
 
   constructor() {}
 
-  loadData(event) {
-    setTimeout(() => {
-      console.log('Done');
-      event.target.complete();
 
-      // App logic to determine if all data is loaded
-      // and disable the infinite scroll
-      if (data.length == 1000) {
-        event.target.disabled = true;
-      }
-    }, 500);
-  }
-
-  toggleInfiniteScroll() {
-    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
-  }
 }
