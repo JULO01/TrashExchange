fb6bea07602c2017b31029c091f373b1fde06cbe
Leon <l.kniffki@t-online.de>
2021-09-25 16:04:47 +0200

    Login works with email and google

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
 
-  constructor(public modalController: ModalController) { }
+export class LoginPage implements OnInit {
+  email: string = '';
+  password: string = '';
+
+  constructor(public modalController: ModalController, public auth: AngularFireAuth) { }
 
   ngOnInit() {
   }
-  close () {
-    console.log("test");
+
+  closeModal () {
     this.modalController.dismiss();
+  }
+
+  signinGoogle() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      this.closeModal();
+      // ...
+    })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  }
+
+
+  signin() {
+    
+    this.auth.signInWithEmailAndPassword("sda23afsaf@gmail.com", "123456")
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+      this.closeModal();
+      // ...
+    })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
   }
 
 }
