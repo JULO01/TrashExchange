c0ba017db6f1966f0a5e2fabf91d6b2f77d7fb9b
JULO01 <90795588+JULO01@users.noreply.github.com>
2021-09-25 17:46:35 +0200

    deleted impoert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { saveConfig } from '@ionic/core';
 import { Observable } from 'rxjs';
-import { firebaseConfig } from 'src/environments/firebase';
 
 @Component({
   selector: 'app-tab1',
